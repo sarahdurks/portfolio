dee4393a7c86844d2dea5e983c2b9fa573a45876
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-06-21 21:45:03 -0700

    commenting out materialUI default examples in components for now

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,20 +1,20 @@
 
-// input
-<form className={classes.root} noValidate autoComplete="off">
-  <Input defaultValue="Hello world" inputProps={{ 'aria-label': 'description' }} />
-  <Input placeholder="Placeholder" inputProps={{ 'aria-label': 'description' }} />
-  <Input defaultValue="Disabled" disabled inputProps={{ 'aria-label': 'description' }} />
-  <Input defaultValue="Error" error inputProps={{ 'aria-label': 'description' }} />
-</form>
+// // input
+// <form className={classes.root} noValidate autoComplete="off">
+//   <Input defaultValue="Hello world" inputProps={{ 'aria-label': 'description' }} />
+//   <Input placeholder="Placeholder" inputProps={{ 'aria-label': 'description' }} />
+//   <Input defaultValue="Disabled" disabled inputProps={{ 'aria-label': 'description' }} />
+//   <Input defaultValue="Error" error inputProps={{ 'aria-label': 'description' }} />
+// </form>
 
-// color
-<form className={classes.root} noValidate autoComplete="off">
+// // color
+// <form className={classes.root} noValidate autoComplete="off">
  
-  <TextField
-    id="filled-secondary"
-    label="Filled secondary"
-    variant="filled"
-    color="secondary"
-  />
+//   <TextField
+//     id="filled-secondary"
+//     label="Filled secondary"
+//     variant="filled"
+//     color="secondary"
+//   />
 
-</form>
+// </form>
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,8 @@
+{/* <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+  <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+  <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+  <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+  <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+</BottomNavigation> */}
+
+// https://material-ui.com/components/bottom-navigation/#api
