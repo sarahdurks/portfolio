08964bec019b32f7d158c010acd50912a291a35d
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-06-22 06:57:43 -0700

    project inclusion updaes

--- src/utils/portfolio.js
+++ src/utils/portfolio.js
@@ -1,61 +1,64 @@
 const Portfolio = {
 
-    https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367456/scheduler_dcgb5y.png
-    https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367456/scheduler_dcgb5y.png
-    https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367456/weather_qvzc6q.png
-    https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367456/budget-pwa_auqalo.png
-    https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367457/pokemon-one_ntbza4.png
-    https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367456/pokemon-fullstack_enra4s.png
 
 
     project:[
     {
         title: `THE POKÉMON DRAFT`,
-        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1623898415/Pokedraft_d6nahg.png`,
+        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367456/pokemon-fullstack_enra4s.png`,
         description: `A simple game to build a Pokémon team of six based on available stats, similar to Fantasy Football.`,
         builtwith: `Node.js / Express.js / MySQL2 / Sequelize / Handlebars`,
         deployed: `https://pokemon-fullstack.herokuapp.com/`,
         github: `https://github.com/sarahdurks/fullstack-pokemon-game`,
       
     },
+
+    {
+        title: `PWA`,
+        image: `
+        https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367456/budget-pwa_auqalo.png`,
+        description: `PWA.`,
+        builtwith: `XX`,
+        deployed: `XX`,
+        github: `XX`,
+      
+    },
+
+    {
+        title: `Pokemon 1`,
+        image: `
+        
+    https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367457/pokemon-one_ntbza4.png`,
+        description: `PWA.`,
+        builtwith: `XX`,
+        deployed: `XX`,
+        github: `XX`,
+      
+    },
+    {
+        title: `SCHEDULER`,
+        image: `
+        https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367456/scheduler_dcgb5y.png`,
+        description: `PWA.`,
+        builtwith: `XX`,
+        deployed: `XX`,
+        github: `XX`,
+      
+    },
   
     {
-        title: `EMPLOYEE TRACKER`,
-        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1623898415/Employeetracker_ima6g4.png`,
-        description: `A command line application to manage a company's employee database, using Node.js, Inquirer, Console.table and MySQL2.            `,
-        builtwith: `Node.js / Inquirer,  MySQL2 / Console.table`,
-        deployed: `https://drive.google.com/file/d/10L0OugkmrbZm38nLI1rE6iZlz7OD2v3d/view`,
-        github: `https://github.com/sarahdurks/employee-tracker`,
-    },
-    {
-        title: `TEAM PROFILE GENERATOR`,
-        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1623898414/Teambuild_kzthdj.png`,
-        description: `A command line application to dynamically create a webpage of key employee info (names, roles, contact info).`,
-        builtwith: `HTML / CSS / JS / Node / Jest / Inquirer`,
-        deployed: `https://drive.google.com/file/d/1F38szSfuKIKV9lyb8Uqizz9PSe-vUCvd/view?usp=sharing`,
-        github: `https://github.com/sarahdurks/team-profile-generator-2`,
-    },
-    {
         title: `NOTE TAKER`,
-        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1623898414/Notetaker_eto7dj.png`,
+        image: `    https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367456/notes_druqku.png`,
         description: `This is a a simple note-taking app using Express, with CRUD operations and user-DOM interaction.`,
         builtwith: `HTML / CSS / JavaScript / Node / Express`,
         deployed: `https://note-taker-sarah.herokuapp.com/`,
         github: `https://github.com/sarahdurks/express-js-notes`,
        
     },
+
     {
-        title: `RUN BUDDY CLIENT WEBSITE`,
-        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624059704/runbuddy_lnmgig.png`,
-        description: `An early Bootcamp assignment building a responsive demo client website.`,
-        builtwith: `HTML / CSS / JS`,
-        deployed: `https://sarahdurks.github.io/Run-Buddy/`,
-        github: `https://github.com/sarahdurks/run-buddy`,
-       
-    },
-    {
-        title: `WEATHER DASHBOARD`,
-        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624059703/weather_qj5cua.png`,
+        title: `WEATHER DASH`,
+        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1624367456/weather_qvzc6q.png`,
         description: `A weather dashboard that features dynamically updated HTML and CSS using OpenWeather API data.`,
         builtwith: ` HTML / Bootstrap / CSS, JavaScript / Moment.js`,
         deployed: `https://sarahdurks.github.io/weather-new`,
