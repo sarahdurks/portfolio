6e228796bb517fedaa88149f47615b45f4a86b24
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-06-21 23:08:26 -0700

    custom card image example

---
+++ src/utils/contact.js
@@ -0,0 +1,9 @@
+
+const Contact = {
+linkedin: `https://www.linkedin.com/in/sarahdurks`,
+mysite: `https://www.sarahdurkin.com`,
+github: `https://github.com/sarahdurks/`
+}
+
+
+export default Contact
