c6b5bfdd9dc8894e92d9dc33b855fddccddc6e21
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-06-21 21:53:51 -0700

    v1 App js, placeholder Resume component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
+import Portfolio from './components/portfolio/Portfolio';
+import Resume from './components/resume/Resume';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    <React.Fragment>
+
+      <Navbar />
+      <Router>
+        <Switch>
+        <Route path='/' component={Home} exact /> 
+         <Route path='/portfolio' component={Portfolio}/>
+         <Route path='/resume' component={Resume}/>
+         <Route path='/contact' component={Contact}/>
+       </Switch> 
+       </Router>
+       <Footer />
+       </React.Fragment>
+   );
+ }
+ 
 
 export default App;
